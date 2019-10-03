48dec9f768dbafb5a300f7f04a98f325df098d53
SavaJDev <40608391+SavaJDev@users.noreply.github.com>
2019-10-03 06:01:04 +0300

    Add files via upload

---
+++ src/pages/AllCoins.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+               <h1>All cryptocurrencies</h1>
+                  <router-link :to="{name: 'topcoins'}">
+                      <p>Back to toplist cryptocurrency</p>
+                  </router-link>
+               <hr> 
+                 <table class="table table-bordered table-hover">
+                     <thead>
+                       <tr>
+                         <th>Logo</th>
+                         <th>Name</th>
+                         <th>Ticker</th>
+                        </tr>
+                      </thead>
+                       <tbody>
+                           <Coin v-for="(coin, i) in coins"
+                                  :key="i"
+                                  :logo="url + coin.ImageUrl"
+                                  :coinName="coin.FullName"
+                                  :symbol="coin.Name"
+                             />                         
+                      </tbody>
+                  </table>
+               <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Coin from '~/components/Coin';
+    import {mapGetters} from 'vuex';
+    
+    export default {
+        data () {
+            return {
+                url: 'https://www.cryptocompare.com/'
+            }
+        },
+        components: {
+             Coin
+        }, 
+         
+        computed: {
+            ...mapGetters ({
+                coins: 'coins/allCoins'
+            })
+        }
+    }
+</script>
+
+<style>
+    table {
+        text-align: center;
+    }
+</style>
---
+++ src/pages/TopCoins.vue
@@ -0,0 +1,68 @@
+<template>
+     <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+              <h1>Toplist cryptocurrency</h1>
+              <router-link :to="{name: 'allcoins'}">
+                  <p>All cryptocurrencies</p>
+              </router-link>
+               <hr>
+               <table class="table table-bordered table-hover">
+                 <thead>
+                   <tr>
+                     <th>Logo</th>
+                     <th>Name</th>
+                     <th>Ticker</th>
+                     <th>Price(USD)</th>
+                     <th>Mkt. cap.</th>
+                    </tr>
+                  </thead>
+                   <tbody>
+                       <TopCoin v-for="(coin, i) in topCoins"
+                              :key="i"
+                              :logo="url + coin.ImageUrl"
+                              :coinName="coin.FullName"
+                              :symbol="coin.Name"
+                              :dftPrice="coin.Price"
+                              :mktCap="coin.MktCap"
+                         />                         
+                  </tbody>
+               </table>
+               <hr>    
+         </div>
+        </div>
+     </div>
+</template>
+
+<script>
+    import TopCoin from '~/components/TopCoin';
+    import {mapGetters} from 'vuex';
+
+     export default {
+        data() {
+            return {
+                url: 'https://www.cryptocompare.com/'
+            }
+        }, 
+         
+        components:{
+            TopCoin
+        },  
+        
+        methods: {
+            
+        }, 
+         
+        computed: {
+            ...mapGetters ({
+                topCoins: 'topcoins/allCoins'
+            })
+        }
+}
+</script>
+
+<style>
+    table {
+        text-align: center;
+    }
+</style>
